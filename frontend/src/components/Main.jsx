b47e98a3c880e15441a2124f31e6b4cdffc482d9
BassamHajar <b2dndr1@gmail.com>
2020-10-22 14:21:43 +0200

    trying grid stratigy in styling

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-const Main = () => <div className="main">main</div>;
+const Main = () => (
+  <div className="main">
+    <div className="game-board">
+      <div className="gridItem">1</div>
+      <div className="gridItem">2</div>
+      <div className="gridItem">3</div>
+      <div className="gridItem">4</div>
+      <div className="gridItem">5</div>
+    </div>
+  </div>
+);
 
 export default Main;
